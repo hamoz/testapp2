1cd050757f06fd823255386cab14eff866003c7d
hashim <hashim@hashim-HP-xw8600-Workstation>
2013-04-11 12:48:59 +0300

    this is my first git project

---
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author hashim
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("الحمد لله");
+    }
+}
